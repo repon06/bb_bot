eb3058e62314b44a4c376ba0731b9315046bb33d
d.bystrov <>
2025-08-11 14:20:56 +0400

    check tg channel from tg & check messages - signal

--- config.py
+++ config.py
@@ -21,6 +21,7 @@ tg_api_id = os.getenv("TG_API_ID")
 tg_api_hash = os.getenv("TG_API_HASH")
 tg_channel_name = '@MYH_System'  # имя канала
 tg_channel_insider_name = '@Insider_Trade'  # имя канала
+tg_channel_insider_id = -1001515215819# '@Insider_Trade'  # имя канала
 session_name = 'session_name'
 session_insider_account = 'session_insider_account'
 
--- telegram.py
+++ telegram.py
@@ -8,16 +8,22 @@ from telethon.errors import ChannelPrivateError, UsernameInvalidError
 from telethon.tl.types import PeerChannel
 
 from config import session_name, tg_api_id, tg_api_hash, tg_channel_name, session_insider_account, \
-    tg_channel_insider_name
+    LOGGING
 
 # Настраиваем логирование
 logging.basicConfig(level=logging.INFO)
 
 # Регулярное выражение для парсинга сообщения с сигналом
-signal_pattern = r"⚡.*?New Signal\s#([\w-]+)\s+.*\n\s*`?\s*Buy:\s*`?\s*([\d.]+)\s*\n\s*`?\s*TP1:\s*`?\s*([\d.]+)\s*\n\s*`?\s*TP2:\s*`?\s*([\d.]+)\s*\n\s*`?\s*TP3:\s*`?\s*([\d.]+)\s*\n\s*`?\s*TP4:\s*`?\s*([\d.]+)\s*\n\s*`?\s*TP5:\s*`?\s*([\d.]+)\s*\n\s*`?\s*STL:\s*`?\s*([\d.]+)"
+# signal_pattern = r"⚡.*?New Signal\s#([\w-]+)\s+.*\n\s*`?\s*Buy:\s*`?\s*([\d.]+)\s*\n\s*`?\s*TP1:\s*`?\s*([\d.]+)\s*\n\s*`?\s*TP2:\s*`?\s*([\d.]+)\s*\n\s*`?\s*TP3:\s*`?\s*([\d.]+)\s*\n\s*`?\s*TP4:\s*`?\s*([\d.]+)\s*\n\s*`?\s*TP5:\s*`?\s*([\d.]+)\s*\n\s*`?\s*STL:\s*`?\s*([\d.]+)"
+# TODO: поменялся формат с 11/08/2025?
+signal_pattern = (
+    r"(\w+)\s*"  # symbol
+    r".*Направление\s*-\s*(\w+).*?"  # direction
+    r"Вход по рынку\s*-\s*([\d.]+).*?"  # entry price
+    r"Наши цели\s*-\s*([\d., ]+).*?"  # targets
+    r"Стоп\s*-\s*([\d.]+)"  # stop loss
+)
 
-
-# TODO: поменялся формат с 20/06/2025?
 
 async def get_tg_signal(limit=100):
     signals = []
@@ -57,7 +63,7 @@ async def get_tg_signal(limit=100):
     return signals
 
 
-async def get_tg_signals_from_insider_trade(limit=100):
+async def get_tg_signals_from_insider_trade_by_name(tg_channel_insider: str, limit=100):
     signals = []
     if os.path.exists(f"{session_insider_account}.session"):
         logging.info("Сессия найдена, используем сохраненную.")
@@ -70,24 +76,96 @@ async def get_tg_signals_from_insider_trade(limit=100):
                               app_version='1.38.1') as client:
 
         try:
-            channel = await client.get_entity(tg_channel_insider_name)
+            channel = await client.get_entity(tg_channel_insider)
         except ChannelPrivateError:
             print("Вы не подписаны на канал или он приватный. Убедитесь, что ваш аккаунт в TelegramClient подписан.")
         except UsernameInvalidError:
             print("Username канала недействителен. Попробуйте по ID.")
-            dialog = await check_and_get_tg_channel(client, tg_channel_insider_name)
+            dialog = await check_and_get_tg_channel(client, tg_channel_insider)
             if dialog:
                 channel = PeerChannel(dialog.id)
 
+            i = 0
+            async for message in client.iter_messages(channel, limit=limit):
+                message_text = message.text
+                message_text = re.sub(r"[^\w\s.,-]", "", message_text)
+                message_date = message.date
+
+                if LOGGING:
+                    print(f'{i} ({message_date}): {message_text}')
+                i += 1
+
+                match = re.search(signal_pattern, message_text, re.DOTALL)
+                if match:
+                    symbol = match.group(1)
+                    direction = match.group(2)
+                    buy_price = float(match.group(3))
+                    take_profits = [float(x.strip()) for x in match.group(4).split(',')]
+                    stop_loss = float(match.group(5))
+
+                    signal_data = {
+                        'symbol': symbol,
+                        'buy_price': buy_price,
+                        'take_profits': take_profits,
+                        'stop_loss': stop_loss,
+                        'date': message_date
+                    }
+                    signals.append(signal_data)
+                    # logging.info(f"Новый сигнал: {signal_data}")
+    return signals
+
+
+async def get_tg_signals_from_insider_trade_by_id(tg_channel_insider_id: int, limit=100):
+    signals = []
+    if os.path.exists(f"{session_insider_account}.session"):
+        logging.info("Сессия найдена, используем сохраненную.")
+    else:
+        logging.info("Сессия не найдена, потребуется авторизация.")
+
+    async with TelegramClient(session_insider_account, tg_api_id, tg_api_hash,
+                              system_version='1.38.1',
+                              device_model='xiaomi',
+                              app_version='1.38.1') as client:
+
+        channel = PeerChannel(tg_channel_insider_id)
+
+        i = 0
         async for message in client.iter_messages(channel, limit=limit):
             message_text = message.text
+            message_text = re.sub(r"[^\w\s.,-]", "", message_text)
             message_date = message.date
 
-            print(f'{message_text}')
-    return []
+            if LOGGING:
+                print(f'{i} ({message_date}): {message_text}')
+            i += 1
+
+            match = re.search(signal_pattern, message_text, re.DOTALL)
+            if match:
+                symbol = match.group(1)
+                direction = match.group(2)
+                buy_price = float(match.group(3))
+                take_profits = [float(x.strip()) for x in match.group(4).split(',')]
+                stop_loss = float(match.group(5))
+
+                signal_data = {
+                    'symbol': symbol,
+                    'buy_price': buy_price,
+                    'take_profits': take_profits,
+                    'stop_loss': stop_loss,
+                    'date': message_date
+                }
+                signals.append(signal_data)
+                # logging.info(f"Новый сигнал: {signal_data}")
+    return signals
 
 
 async def check_and_get_tg_channel(client: TelegramClient, tg_name_find: str):
+    '''
+    Поиск нужного канала и его ID
+    :param client:
+    :param tg_name_find:
+    :return:
+    '''
     dialogs = await client.get_dialogs()
 
     channels = [d for d in dialogs if d.is_channel]
@@ -109,8 +187,11 @@ async def check_and_get_tg_channel(client: TelegramClient, tg_name_find: str):
 
 # Запуск асинхронной функции
 if __name__ == "__main__":
-    signals = asyncio.run(get_tg_signal(limit=50))
-    logging.info(f"Получено {len(signals)} сигналов.")
+    # signals = asyncio.run(get_tg_signal(limit=50))
+    # logging.info(f"1) Получено {len(signals)} сигналов.")
 
-    signals = asyncio.run(get_tg_signals_from_insider_trade(limit=50))
-    logging.info(f"Получено {len(signals)} сигналов.")
+    signals = asyncio.run(get_tg_signals_from_insider_trade_by_name(tg_channel_insider_name, limit=50))
+    logging.info(f"2) Получено {len(signals)} сигналов.")
+
+# signals = asyncio.run(get_tg_signals_from_insider_trade_by_id(tg_channel_insider_id, limit=50) )
+# logging.info(f"2) Получено {len(signals)} сигналов.")
